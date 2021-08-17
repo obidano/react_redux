991e798ef96aeec99405aa222ad5c7b385953e6e
obidano <omotetedan2@gmail.com>
2021-08-17 09:41:02 +0100

    project config #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import React, {Component} from "react";
+import {Button, Col, Form, FormControl, Row} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+    render() {
+        return (
+            <div className='container'>
+                <Form inline>
+                    <h2>Saisir votre date d'anniversaire</h2>
+                    <Row>
+                        <Col className="col-md-5 mx-auto">
+                            <FormControl type="date"
+                            />
+                            <Button className='mt-3'>
+                                Submit
+                            </Button>
+                        </Col>
+
+
+                    </Row>
+
+
+                </Form>
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
