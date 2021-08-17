f8de2b41c7c98589dabda31240b0c4f3abba0b38
obidano <omotetedan2@gmail.com>
2021-08-17 12:01:23 +0100

    reducers and store redux #8

--- src/reducers/actions/actions.js
+++ src/reducers/actions/actions.js
@@ -1,21 +1,12 @@
-
-
-
 export const ADD_CHARACTER = 'ADD_CHARACTER';
 export const NEW_CHARACTER = 'NEW_CHARACTER';
 
 export class MyActions {
-    static addCharacterById(id) {
-        return {
-            type: ADD_CHARACTER,
-            id
-        }
+    static addCharacterById(dispatch, id) {
+        dispatch({type: ADD_CHARACTER, id})
     }
 
-    static createCharacter(id) {
-        return {
-            type: NEW_CHARACTER,
-            id
-        }
+    static createCharacter(dispatch, id) {
+        dispatch({type: NEW_CHARACTER, id})
     }
 }
--- src/supersquad/SuperSquad.js
+++ src/supersquad/SuperSquad.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {MyActions} from "../reducers/actions/actions";
+import {Button, Col, Row} from "react-bootstrap";
 
 
 class SuperSquad extends Component {
@@ -15,9 +16,37 @@ class SuperSquad extends Component {
     }
 
     render() {
-        return "SUperSquad!!!!"
+        return <div className="container">
+
+            <Row>
+                <Col className="col-md-4 mx-auto">
+                    <Button onClick={() => {
+                        const {dispatch} = this.props;
+                        if (this.props.app.length === 0) return
+                        MyActions.createCharacter(dispatch, this.props.app.length - 1);
+                        MyActions.addCharacterById(dispatch, this.props.app.length - 1);
+                    }}>Add Heros</Button>
+                </Col>
+            </Row>
+
+            <Row>
+                <div className="col-md-6">
+                    {this.props.app.map((d, k) => {
+                        return <div key={k}>{d.name}</div>
+                    })}
+                </div>
+
+                <div className="col-md-6">
+                    {this.props.heroes.map((d, k) => {
+                        return <div key={k}>{d.name}</div>
+                    })}
+                </div>
+
+            </Row>
+
+        </div>
     }
 }
 
 // const mapState = (state) => state
-export default connect( (state) => state, {MyActions})(SuperSquad);
+export default connect((state) => state, null)(SuperSquad);
