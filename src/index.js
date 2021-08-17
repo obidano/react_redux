56cdb104055500ee3a1b5951328e87be092cdb3b
obidano <omotetedan2@gmail.com>
2021-08-17 10:24:50 +0100

    reducers and store redux #5

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducers from "./reducers";
 // import reportWebVitals from './reportWebVitals';
+
+const store = createStore(reducers);
+console.log("store STATE", store.getState());
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+
     </React.StrictMode>,
     document.getElementById('root')
 );
