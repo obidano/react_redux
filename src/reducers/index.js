8a7f06a680230535c865148441d4081dc4e8c37b
obidano <omotetedan2@gmail.com>
2021-08-17 10:21:12 +0100

    reducers #4

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import characters from '../data/characters.json';
+
+const reducers = (state = characters, action) => {
+
+    switch (action.type) {
+        default:
+            return state
+    }
+
+}
+
+export default reducers
+
